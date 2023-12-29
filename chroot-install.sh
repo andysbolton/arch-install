38ed9e92ee37b2d7c02932591c155a0074bfc773
Andy Bolton <andy.bolton@smartwyre.com>
2023-12-29 02:17:03 -0700

    Some refactors

--- chroot-install.sh
+++ chroot-install.sh
@@ -16,15 +16,17 @@ echo "LANG=en_US.UTF-8" >/etc/locale.conf
 
 echo "Setting hostname..."
 echo "$host" >/etc/hostname
-sed "s/HOSTNAME/$host/g" etc/hosts /etc/hosts
+sed "s/%HOSTNAME%/$host/g" etc/hosts /etc/hosts
 
 echo "Setting root password..."
 passwd
 
 echo "Installing bootloader..."
+uuid=$(blkid -s UUID -o value /dev/sda3)
 bootctl --path=/boot install
 cp boot/loader/loader.conf /boot/loader/loader.conf
 cp boot/loader/entries/arch.conf /boot/loader/entries/arch.conf
+sed -i "s/%UUID%/$uuid/" /boot/loader/entries/arch.conf
 
 echo "Setting up systemd-resoved..."
 systemctl enable systemd-resolved.service
@@ -35,7 +37,7 @@ interface="wlp2s0"
 echo "Setting up systemd-networkd..."
 wpa_passphrase "$ssid" "$passphrase" >"/etc/wpa_supplicant/wpa_supplicant-$interface.conf"
 systemctl enable wpa_supplicant@"$interface"
-sed "s/INTERFACE/$interface/" etc/systemd/network/default.network >/etc/systemd/network/default.network
+sed "s/%INTERFACE%/$interface/" etc/systemd/network/default.network >/etc/systemd/network/default.network
 systemctl enable systemd-networkd.service
 
 echo "Exiting chroot..."
