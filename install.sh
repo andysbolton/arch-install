778751c3a242e5a50d9b176baa726cda9e4af74e
Andy Bolton <andy.bolton@smartwyre.com>
2023-12-29 02:18:31 -0700

    Uncomment script

--- install.sh
+++ install.sh
@@ -2,10 +2,12 @@
 
 set -e
 
+echo "Testing internet connection..."
 if ! ping -c 3 google.com >/dev/null; then
     echo "No internet connection."
     exit 1
 fi
+echo "Connected."
 
 if [ -z "$PASSPHRASE" ]; then
     echo "PASSPHRASE is not set."
@@ -19,20 +21,20 @@ fi
 
 read -r -p "Enter hostname: " host
 
-# echo "Updating system clock..."
-# timedatectl set-ntp true
-#
-# echo "Optimizing mirror list..."
-# reflector -c 'US' -a 15 -p https --sort rate --save /etc/pacman.d/mirrorlist
-# pacman -Syy
-#
-# echo "Installing base system..."
-# pacstrap /mnt base base-devel linux linux-firmware \
-#     neovim less which man-db man-pages intel-ucode \
-#     wpa_supplicant fish iw git
-#
-# echo "Generating fstab..."
-# genfstab -U /mnt >>/mnt/etc/fstab
+echo "Updating system clock..."
+timedatectl set-ntp true
+
+echo "Optimizing mirror list..."
+reflector -c 'US' -a 15 -p https --sort rate --save /etc/pacman.d/mirrorlist
+pacman -Syy
+
+echo "Installing base system..."
+pacstrap /mnt base base-devel linux linux-firmware \
+    neovim less which man-db man-pages intel-ucode \
+    wpa_supplicant fish iw git
+
+echo "Generating fstab..."
+genfstab -U /mnt >>/mnt/etc/fstab
 
 mkdir -p /mnt/setup
 cp chroot-install.sh /mnt/setup/chroot-install.sh
