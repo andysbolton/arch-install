139b99d75657578e16be4e5c11ad1551baacba75
Andy Bolton <andy.bolton@smartwyre.com>
2023-12-29 00:19:13 -0700

    Try to refactor chroot commands to separate script

---
+++ chroot-install.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+echo "Setting timezone..."
+ln -sf /usr/share/zoneinfo/US/Mountain /etc/localtime
+
+hwclock --systohc
+
+echo "Setting locale..."
+sed -i 's/#en_US.UTF-8/en_US.UTF-8/' /etc/locale.gen
+local-gen
+echo "LANG=en_US.UTF-8" >/etc/locale.conf
+
+echo "Setting hostname..."
+echo "$host" >/etc/hostname
+sed "s/HOSTNAME/$host/g" etc/hosts /etc/hosts
+
+echo "Setting root password..."
+passwd
+
+echo "Installing bootloader..."
+bootctl --path=/boot install
+cp boot/loader/loader.conf /boot/loader/loader.conf
+cp boot/loader/entries/arch.conf /boot/loader/entries/arch.conf
+
+echo "Setting up systemd-resoved..."
+# We'll create the following symlink outside of the chroot once we've exited it at the end of the script.
+# Otherwise, per https://wiki.archlinux.org/title/Systemd-resolved, the link won't be persisted.
+# ln -sf ../run/systemd/resolve/stub-resolv.conf /mnt/etc/resolv.conf
+systemctl enable systemd-resolved.service
+
+# Requery the interface name as systemd is now in use which alters it.
+interface=$(iw dev | awk '$1=="Interface"{print $2}')
+
+echo "Setting up systemd-networkd..."
+wpa_passphrase "$ssid" "$passphrase" >"/etc/wpa_supplicant/wpa_supplicant-$interface.conf"
+systemctl enable wpa_supplicant@"$interface"
+sed "s/INTERFACE/$interface/" etc/systemd/network/default.network >/etc/systemd/network/default.network
+systemctl enable systemd-networkd.service
+
+echo "Exiting chroot..."
--- install.sh
+++ install.sh
@@ -25,48 +25,10 @@ pacstrap /mnt base base-devel linux linux-firmware \
 echo "Generating fstab..."
 genfstab -U /mnt >>/mnt/etc/fstab
 
+cp chroot-install.sh /mnt
 echo "chroot into new system"
-arch-chroot /mnt
-
-echo "Setting timezone..."
-ln -sf /usr/share/zoneinfo/US/Mountain /etc/localtime
-
-hwclock --systohc
-
-echo "Setting locale..."
-sed -i 's/#en_US.UTF-8/en_US.UTF-8/' /etc/locale.gen
-local-gen
-echo "LANG=en_US.UTF-8" >/etc/locale.conf
-
-echo "Setting hostname..."
-echo "$host" >/etc/hostname
-sed "s/HOSTNAME/$host/g" etc/hosts /etc/hosts
-
-echo "Setting root password..."
-passwd
-
-echo "Installing bootloader..."
-bootctl --path=/boot install
-cp boot/loader/loader.conf /boot/loader/loader.conf
-cp boot/loader/entries/arch.conf /boot/loader/entries/arch.conf
-
-echo "Setting up systemd-resoved..."
-# We'll create the following symlink outside of the chroot once we've exited it at the end of the script.
-# Otherwise, per https://wiki.archlinux.org/title/Systemd-resolved, the link won't be persisted.
-# ln -sf ../run/systemd/resolve/stub-resolv.conf /mnt/etc/resolv.conf
-systemctl enable systemd-resolved.service
-
-# Requery the interface name as systemd is now in use which alters it.
-interface=$(iw dev | awk '$1=="Interface"{print $2}')
-
-echo "Setting up systemd-networkd..."
-wpa_passphrase "$ssid" "$passphrase" >"/etc/wpa_supplicant/wpa_supplicant-$interface.conf"
-systemctl enable wpa_supplicant@"$interface"
-sed "s/INTERFACE/$interface/" etc/systemd/network/default.network >/etc/systemd/network/default.network
-systemctl enable systemd-networkd.service
-
-echo "Exiting chroot..."
-exit
+arch-chroot /mnt "./install.sh" "$host" "$ssid" "$passphrase"
+rm /mnt/chroot-install.sh
 
 echo "Setting stub link for systemd-resolved..."
 ln -sf ../run/systemd/resolve/stub-resolv.conf /mnt/etc/resolv.conf
