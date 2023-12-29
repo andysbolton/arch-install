2adc5435238166ea66563ea4c4d1c1d659c342b4
Andy Bolton <andy.bolton@smartwyre.com>
2023-12-29 00:25:31 -0700

    Debug: comment out lines

--- install.sh
+++ install.sh
@@ -5,29 +5,29 @@ set -e
 interface=$(iw dev | awk '$1=="Interface"{print $2}')
 
 read -r -p "Enter hostname: " host
-read -r -p "Enter SSID: " ssid
-read -r -p "Enter network passphrase: " passphrase
+# read -r -p "Enter SSID: " ssid
+# read -r -p "Enter network passphrase: " passphrase
 
-iwctl --passphrase "$passphrase" station "$interface" connect "$ssid"
-
-echo "Updating system clock..."
-timedatectl set-ntp true
-
-echo "Optimizing mirror list..."
-reflector -c 'US' -a 15 -p https --sort rate --save /etc/pacman.d/mirrorlist
-pacman -Syy
-
-echo "Installing base system..."
-pacstrap /mnt base base-devel linux linux-firmware \
-    neovim less which man-db man-pages intel-ucode \
-    wpa_supplicant fish iw git
-
-echo "Generating fstab..."
-genfstab -U /mnt >>/mnt/etc/fstab
-
-cp chroot-install.sh /mnt
-echo "chroot into new system"
-arch-chroot /mnt "./install.sh" "$host" "$ssid" "$passphrase"
+# iwctl --passphrase "$passphrase" station "$interface" connect "$ssid"
+#
+# echo "Updating system clock..."
+# timedatectl set-ntp true
+#
+# echo "Optimizing mirror list..."
+# reflector -c 'US' -a 15 -p https --sort rate --save /etc/pacman.d/mirrorlist
+# pacman -Syy
+#
+# echo "Installing base system..."
+# pacstrap /mnt base base-devel linux linux-firmware \
+#     neovim less which man-db man-pages intel-ucode \
+#     wpa_supplicant fish iw git
+#
+# echo "Generating fstab..."
+# genfstab -U /mnt >>/mnt/etc/fstab
+#
+# cp chroot-install.sh /mnt
+# echo "chroot into new system"
+arch-chroot /mnt "./chroot-install.sh" "$host" "$SSID" "$PASSPHRASE"
 rm /mnt/chroot-install.sh
 
 echo "Setting stub link for systemd-resolved..."
