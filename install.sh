b8a4796a87c25fce2569619ee3f8cd8692c930ae
Andy Bolton <andy.bolton@smartwyre.com>
2023-12-29 00:40:50 -0700

    Refactoring setup

--- install.sh
+++ install.sh
@@ -4,6 +4,16 @@ set -e
 
 interface=$(iw dev | awk '$1=="Interface"{print $2}')
 
+if [ -z "$PASSPHRASE" ]; then
+    echo "PASSPHRASE is not set."
+    exit 1
+fi
+
+if [ -z "$SSID" ]; then
+    echo "SSID is not set."
+    exit 1
+fi
+
 read -r -p "Enter hostname: " host
 # read -r -p "Enter SSID: " ssid
 # read -r -p "Enter network passphrase: " passphrase
@@ -25,10 +35,12 @@ read -r -p "Enter hostname: " host
 # echo "Generating fstab..."
 # genfstab -U /mnt >>/mnt/etc/fstab
 #
-cp chroot-install.sh /mnt/chroot-install.sh
+cp chroot-install.sh /mnt/setup/chroot-install.sh
+cp -r boot/ /mnt/setup/boot/
+cp -r etc/ /mnt/setup/etc/
 echo "chroot into new system"
-arch-chroot /mnt "./chroot-install.sh" "$host" "$SSID" "$PASSPHRASE"
-rm /mnt/chroot-install.sh
+arch-chroot /mnt "cd setup && ./chroot-install.sh" "$host" "$SSID" "$PASSPHRASE"
+rm -rf /mnt/setup
 
 echo "Setting stub link for systemd-resolved..."
 ln -sf ../run/systemd/resolve/stub-resolv.conf /mnt/etc/resolv.conf
